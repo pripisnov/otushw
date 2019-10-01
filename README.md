528f3abe1c54b3a5b9efd92f9f2d486cd7d6a168
Andrey Pripisnov <pripisnov@bk.ru>
2019-10-01 22:22:33 +0300

    hw01-maven

---
+++ README.md
@@ -0,0 +1,1 @@
+**OUTS Java** homework repo
---
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,12 @@
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        List<String> myList = Lists.newArrayList("Hello,", " ", "Otus!");
+        String result = Joiner.on("").join(myList);
+        System.out.println(result);
+    }
+}
